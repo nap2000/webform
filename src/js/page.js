c8300d1398244cbb35de7d6b902102277a9802f8
Neil Penman <neilpenman@gmail.com>
2021-06-03 10:06:25 +1000

    Modify page view to include draft button on every page

--- src/js/page.js
+++ src/js/page.js
@@ -9,6 +9,7 @@ import events from './event';
 import config from 'enketo/config';
 import { getSiblingElements, getAncestors } from './dom-utils';
 import 'jquery-touchswipe';
+import {t} from "./translator";
 
 export default {
     /**
@@ -415,11 +416,15 @@ export default {
         this.$btnPrev.add( this.$btnFirst ).toggleClass( 'disabled', !prev );
         this.$formFooter.toggleClass( 'end', !next );
 
-        // smap hide the submit form button and powered by enketo logo except on last page
+        // smap hide the submit form button and powered by enketo logo except on last page. Show the draft button on non final pages
         if(next) {
-            $('#submit-form, .enketo-power, .draft').hide();
+            $('.form-footer [name="draft"]').prop('checked', true);
+            $('.enketo-power, .draft').hide();
+            $('#submit-form').text(t( 'formfooter.savedraft.btn' )).show();
         } else {
-            $('#submit-form, .enketo-power, .draft').show();
+            $('.form-footer [name="draft"]').prop('checked', false);
+            $('#submit-form').text(t( 'formfooter.submit.btn' )).show();
+            $('.enketo-power, .draft').show();
         }
         // end smap
     },
