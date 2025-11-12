d190c816a884828520af00d80583a35c60675d32
Neil Penman <neilpenman@gmail.com>
2025-11-12 11:09:53 +1000

    Update dep script and add package-lock.json

--- dep.sh
+++ dep.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-cp build/webFormTest.html /Library/Webserver/Documents
-cp build/js/webform-bundle.js /Library/Webserver/Documents/build/js
+
+echo "building webforms"
+grunt develop
+
+cp ~/git/webform/build/js/enketo-bundle.js WebContent/build/js/webform-bundle.js
+
+if [ "$1" != develop ]
+then
+	rm WebContent/build/js/webform-bundle.min.js
+
+        # uglify
+        pushd ~/git/webform
+        grunt minify
+        popd
+
+	cp ~/git/webform/build/js/bundle.min.js WebContent/build/js/webform-bundle.min.js
+
+else
+        # Rename the non minified version so that it can be used
+	cp ~/git/webform/build/js/enketo-bundle.js WebContent/build/js/webform-bundle.min.js
+fi
+
+./enk_up.sh
