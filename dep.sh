b1a4d47410f7e37a5ef38ad5b50ab02dd0ef702a
Neil Penman <neilpenman@gmail.com>
2026-06-05 18:12:36 +1000

    Phase 1 - Add bootstrap

--- dep.sh
+++ dep.sh
@@ -18,6 +18,7 @@ else
     cp build/js/zxing-bundle.js $smapServer/WebContent/build/js/zxing-bundle.js
 fi
 
+cp build/js/bootstrap.bundle.min.js $smapServer/WebContent/build/js/bootstrap.bundle.min.js
 cp build/css/* $smapServer/WebContent/build/css
 cp build/fonts/* $smapServer/WebContent/build/fonts
 
